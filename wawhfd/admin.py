1b060eda6f6e7dbde6dd222d47ca5f04768ed403
Caleb Rash <caleb@brilliant.org>
2016-09-03 07:50:51 -0500

    Update admim

--- wawhfd/admin.py
+++ wawhfd/admin.py
@@ -6,7 +6,10 @@ class RecipeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description', 'link', 'deleted',)
 
 class CalendarEntryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'link', 'deleted',)
+    list_display = ('id', 'date', 'recipe_str',)
+
+    def recipe_str(self, entry):
+        return 'Recipe: {name} ({id})'.format(name=entry.recipe.name, id=entry.recipe.id)
 
 admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(CalendarEntry)
+admin.site.register(CalendarEntry, CalendarEntryAdmin)
